686574d3fce6403499c4a2cc740e64b366aed55e
Artur Carneiro Tavares <167725530+carneiroo1@users.noreply.github.com>
2026-06-12 14:30:12 -0300

    name on html

--- static/js/chat.js
+++ static/js/chat.js
@@ -16,9 +16,16 @@ function enviarMensagem() {
     input.value = "";
 }
 
+// OUVINTE CORRIGIDO AQUI:
 socket.on("receive_message", function(data) {
     const list = document.getElementById("message-list");
     const item = document.createElement("li");
-    item.textContent = data.message;
+    const strong = document.createElement("strong");
+    strong.textContent = data.username;
+    const br = document.createElement("br");
+    const textoMensagem = document.createTextNode(` ${data.message}`);
+    item.appendChild(strong);
+    item.appendChild(br);
+    item.appendChild(textoMensagem);
     list.appendChild(item);
 });
